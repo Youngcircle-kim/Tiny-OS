da77095867ebc5419be4b24dba0a393d43233574
youngcircle-kim <dudghks021121@gmail.com>
2025-12-09 21:04:55 +0900

    [FEAT] Reading the disk

--- bootloader/stage1/bootstage1.asm
+++ bootloader/stage1/bootstage1.asm
@@ -18,4 +18,31 @@ Stage1_entrypoint:
         ;   ------------------------------
         ;   Set up a temporary stack
         ;   ------------------------------
+        ;  SS:SP = 0x0000:Stage1_entrypoint
+        mov sp, Stage1_entrypoint
+
+        ; Clear direction flag
+        cld
+
+        ; Loading stage 2 from disk into RAM
+        mov [disk], dl         ;    Storing disk number.
+        mov ax, (stage2_start-stage1_start)/512     ;   ax : start sector
+        mov cx, (kernel_end-stage2_start)/512       ;   cx : number of sectors to read
+        mov bx, stage2_start    ;   bx : offset of buffer
         
+        xor dx, dx                ; dx : segment of buffer
+
+        call Real_mode_read_disk
+
+        mov si, stage1_message
+        call Real_mode_println
+
+    .halt:  ; Infinite loop
+        hlt
+        jmp .halt
+
+%include "stage1/disk.asm"  
+%include "stage1/print.asm"  
+
+times 510-($-$$) db 0    ; Padding
+dw 0xAA55                       ; 마지막 2바이트는 반드시 0xAA55 여야 BIOS가 부팅 가능한 디스크로 인식.        
